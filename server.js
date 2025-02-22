5c91bb0dd181767d9d48f04ce6c691c9a13a8705
Khurshid <razakovkh.6774@gail.com>
2025-02-22 13:22:51 +0900

    feat: develope master router ejs page

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 console.log("Web serverni boshlash");
-
-// npm install express --save => express yuklash
-// npm i express => qisqartmasi
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const http = require("http");
+const fs = require("fs");
 
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data)
+    }
+});
 // 1
 app.use(express.static("public"));
 app.use(express.json());
@@ -14,25 +21,20 @@ app.use(express.urlencoded({ extended: true }));
 // 2 Session
 
 // 3 Views code
-// npm i ejs => ejs yuklash. ejs - html frontend dev ni yasash uchun kerak
-// BSSR - backendda htmlni qurish
 app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4 Routing code
-// app.post("/create-item",(req, res) => {
-//     console.log(req,body);
-//     res.json({test:"succes"});
-// });
-// app.get("/", function(req, res) {
-//     res.render("harid");
-// });
 app.post("/create-item",(req,res) => {
-    console.log(req);
-    res.json({test:"succes"});
+    // TODO: code with db here
 });
+
+app.get("/master", (req, res) => {
+    res.render("master", {user: user});
+});
+
 app.get("/", function (req, res) {
-    res.render("harid");
+    res.render("harid", );
 });
 
 const server = http.createServer(app);
@@ -40,5 +42,4 @@ let PORT = 3000;
 server.listen(PORT, function () {
     console.log(`The server is running successfully on port: ${PORT}`);
 });
-//git init - gitni yuklash
 
