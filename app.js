a941bea4ac0dcfa90c471d97312702688dee56be
Khurshid <razakovkh.6774@gail.com>
2025-03-04 23:06:11 +0900

    feat: FrontEnd rejalarni o'chirish (delete)

--- app.js
+++ app.js
@@ -1,64 +1,52 @@
-console.log("Web serverni boshlash");
+console.log("Web Serverni boshlash");
 const express = require("express");
 const res = require("express/lib/response");
 const app = express();
 
-// const fs = require("fs");
-
-// let user;
-// fs.readFile("database/user.json", "utf8", (err, data) => {
-//     if(err) {
-//         console.log("ERROR:", err);
-//     } else {
-//         user = JSON.parse(data)
-//     }
-// });
-
-//MongoDB connect
-// MongoDB chaqirish
+//MongoDB chaqirish
 const db = require("./server").db();
+const mongodb = require("mongodb");
 
-
-// 1
+// 1 kirish code
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 // 2 Session
-
 // 3 Views code
 app.set("views", "views");
 app.set("view engine", "ejs");
-
 // 4 Routing code
-app.post("/create-item",(req, res) => {
-    // console.log(req.body);
+app.post("/create-item", (req, res) => {
     console.log("user entered /create-item");
     const new_reja = req.body.reja;
-    db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
+    db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
         console.log(data.ops);
         res.json(data.ops[0]);
     });
-    // TODO: code with db here
 });
 
-// app.get("/master", (req, res) => {
-//     res.render("master", {user: user});
-// });
-
-app.get("/", function (req, res) {
-    console.log('user entered /');
-    db.collection("plans")
+app.post("/delete-item", (req, res) => {
+    const id = req.body.id;
+    db.collection("plans").deleteOne(
+        { _id: new mongodb.ObjectId(id) },
+        function (err, data) {
+            res.json({ state: "success" });
+        }
+        );
+    });
+    
+    app.get("/", function (req, res) {
+        console.log("user entered /");
+        db.collection("plans")
         .find()
         .toArray((err, data) => {
-            if(err) {
+            if (err) {
                 console.log(err);
                 res.end("something went wrong");
             } else {
-                // console.log(data);
                 res.render("reja", { items: data });
             }
         });
-});
-
-module.exports = app;
+    });
+    
+    module.exports = app;
--- public/browser.js
+++ public/browser.js
@@ -39,50 +39,8 @@ document.getElementById("create-form").addEventListener("submit", function (e) {
     });
 });
 
-/*console.log("Fronted js ishga tushdi");
-
-function itemTemplate(item) {
-    return ` <li
-    class="list-group-item list-group-item-info d-flex align-items-center justify-content-between"
-    >
-    <span class="item-text">${item.reja}</span>
-    <div>
-    <button
-    data-id="${item._id}"
-    class="edit-me btn btn-secondary btn-sm mr-1"
-    >O'zgartirish
-    </button>
-    <button
-    data-id="${item._id}"
-    class="delete-me btn btn-danger btn-sm"
-    >O'chirish
-    </button>
-    </div>
-    </li>`;
-}
-
-let createField = document.getElementById("create-field");
-
-document
-.getElementById("create-form").addEventListener("submit", function (event) {
-    event.preventDefault();
-    
-    axios
-    .post("/create-item",{reja: createField.value})
-    .then((response) => {
-        document
-        .getElementById("item-list")
-        .insertAdjacentHTML("beforeend", itemTemplate(response.data));
-        createField.value = "";
-        createField.focus();
-    })
-    .catch((err) => {
-        console.log("Iltimos qaytadan harakat qiling!");
-    });
-});
-
 document.addEventListener("click", function (event) {
-    //delete
+    //delete oper
     console.log(event.target);
     if(event.target.classList.contains("delete-me")) {
         if(confirm("Aniq o'chirmoqchimisiz")) {
@@ -98,9 +56,8 @@ document.addEventListener("click", function (event) {
         } 
     }
     
-    //edit
+    //edit oper
     if(event.target.classList.contains("edit-me")) {
         alert("siz edit tugmasini bosdingiz");
     }
 }); 
-*/
