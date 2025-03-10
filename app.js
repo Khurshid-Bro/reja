69c70a9a81a0353df0f67d9163f93de5316e1292
Khurshid <razakovkh.6774@gail.com>
2025-03-11 00:01:57 +0900

    feat port 3001

--- app.js
+++ app.js
@@ -3,30 +3,45 @@ const express = require("express");
 const res = require("express/lib/response");
 const app = express();
 
-//MongoDB chaqirish
+// const fs = require("fs");
 
+// let user;
+// fs.readFile("database/user.json", "utf8", (err, data) => {
+//   if (err) {
+//     console.log("ERROR:", err);
+//   } else {
+//     user = JSON.parse(data);
+//   }
+// });
+
+// MongoDB Connect
 const db = require("./server").db();
-// const db = client.db();
 const mongodb = require("mongodb");
 
-// 1 kirish code
+// 1 Kirish code
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// 2 Session code - AUTHENTICATION (Login, Signup)
+// 2 Session code
 
-// 3 Views code - (B)SSR - Backend ishida HTML qurib yuborish. SPI - Backend and Frontent alohida ishlaydi.
+// 3 Views code
 app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4 Routing code
 app.post("/create-item", (req, res) => {
     console.log("user entered /create-item");
+    console.log(req.body);
     const new_reja = req.body.reja;
     db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
         console.log(data.ops);
         res.json(data.ops[0]);
+        // if (err) {
+        //   console.log("something went wrong");
+        // } else {
+        //   res.end("successfully added");
+        // }
     });
 });
 
@@ -39,40 +54,44 @@ app.post("/delete-item", (req, res) => {
         }
         );
     });
-
+    
     app.post("/edit-item", (req, res) => {
         const data = req.body;
         console.log(data);
         db.collection("plans").findOneAndUpdate(
-            { _id: new mongodb.ObjectId(data.id) }, 
-            { $set: { reja: data.new_input } }, 
-            function (err, data) {
+            { _id: new mongodb.ObjectId(data.id) },
+            { $set: { reja: data.new_input } },
+            function (err, dta) {
                 res.json({ state: "success" });
             }
-        );
-    });
-
-    app.post("/delete-all", (req, res) => {
-        if(req.body.delete_all) {
-            db.collection("plans").deleteMany(function() {
-                res.json({ state: "hamma rejalar ochirildi"})
-            });
-        }
-    });
-    
-    app.get("/", function (req, res) {
-        console.log("user entered to /");
-        db.collection("plans")
-        .find()
-        .toArray((err, data) => {
-            if (err) {
-                console.log(err);
-                res.end("something went wrong");
-            } else {
-                console.log(data);
-                res.render("reja", { items: data });
+            );
+            // res.end("done");
+        });
+        
+        app.post("/delete-all", (req, res) => {
+            if (req.body.delete_all) {
+                db.collection("plans").deleteMany(function () {
+                    res.json({ state: " hamma rejalar o'chirildi" });
+                });
             }
         });
-    });
-    
-    module.exports = app;  
+        
+        app.get("/author", (req, res) => {
+            res.render("author", { user: user });
+        });
+        
+        app.get("/", function (req, res) {
+            console.log("user entered /");
+            db.collection("plans")
+            .find()
+            .toArray((err, data) => {
+                if (err) {
+                    console.log(err);
+                    res.end("something went wrong");
+                } else {
+                    res.render("reja", { items: data });
+                }
+            });
+        });
+        
+        module.exports = app;
